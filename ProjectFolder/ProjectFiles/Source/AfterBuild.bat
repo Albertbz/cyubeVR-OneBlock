e9ff9eff50c634d2c0ef5a8cd127c18d4012605d
Albertbz <mail@albertbz.com>
2022-08-13 11:37:21 +0200

    Updated VoxelAPI

--- ProjectFolder/ProjectFiles/Source/AfterBuild.bat
+++ ProjectFolder/ProjectFiles/Source/AfterBuild.bat
@@ -1,4 +1,4 @@
 cd ../x64/Release
 copy Code.dll ..\..\Code.dll
-copy Code.dll "C:\Program Files (x86)\Steam\steamapps\common\cyubeVR\cyubeVR\Mods\ModFolders\OneBlock\Update 52\APIMods\OneBlock__V1\Code.dll"
+copy Code.dll "C:\Program Files (x86)\Steam\steamapps\common\cyubeVR\cyubeVR\Mods\ModFolders\OneBlock\Update 53\APIMods\OneBlock__V1\Code.dll"
 pause
